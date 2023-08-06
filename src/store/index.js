593d90e74ca36008d05961a2a3555d231eeeb3b4
Alex <sanyaigas88@gmail.com>
2023-08-06 23:35:54 +0300

    Только начало перевода на db.json

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    products: []
   },
   getters: {
+    PRODUCTS (state) {
+      return state.products
+    }
   },
   mutations: {
   },
